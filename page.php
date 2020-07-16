454bf0a67d6c67f3f4ba2ea5bcac805658e86765
Kamil <kamiljasinski1996@gmail.com>
2020-07-16 16:08:36 +0200

    Dodano szablon dla PAGE.php

--- page.php
+++ page.php
@@ -1,8 +1,44 @@
 <?php get_header();  ?>
-<!-- TEN PLIK WYŚWIETLA OSOBNĄ STRONĘ Z PANELU -->
+<?php get_template_part('single_menu'); ?>
 
-THAT IS PAGE
+<!-- TEN PLIK WYŚWIETLA OSOBNY POST-->
+        <?php if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
 
 
+
+<section id="post" class="events-wrapper"><!-- Included the last events -->
+           <h1 class="h1-events" style="border-bottom: 1px solid #151515; margin-bottom: 30px; padding-bottom: 30px;"><?php the_title();?></h1>
+
+   <section class="events">
+<!--          ARTICLE ONE          -->
+
+
+    <article class="event">
+
+            <article class="content-text">
+                <?php the_content();?>
+                <?php edit_post_link( '&raquo; Edit Post', '<p>','</p>'); ?>
+                <div class="post-details">
+               <p>By
+               <a id="post-author" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>"><?php the_author();?></a>
+
+               <?php the_time('F jS, Y g:i a') ?>
+               </p>
+               </div>
+            </article>
+
+    </article>
+  </section><!--EVENTS    -->
+
+</section><!--     --- EVENTS WRAPPER --- END     -->
+
+
+
+
+    <?php  endwhile;
+        else :
+          echo '<p>Sorry, no content found.</p>';
+    endif; ?>
 
 <?php get_footer(); ?>
